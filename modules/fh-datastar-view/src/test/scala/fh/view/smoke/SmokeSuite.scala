56d64635e23fe96d20489221e90a394c6044e926
Per Øyvind Løvik Kanestrøm <per.kanestrom@kartverket.no>
2026-07-13 22:31:20 +0200

    playwright-visual-test-simplify

--- modules/fh-datastar-view/src/test/scala/fh/view/smoke/SmokeSuite.scala
+++ modules/fh-datastar-view/src/test/scala/fh/view/smoke/SmokeSuite.scala
@@ -44,7 +44,17 @@ abstract class SmokeSuite extends munit.FunSuite {
     )
     browser = playwright
       .chromium()
-      .launch(new BrowserType.LaunchOptions().setHeadless(true))
+      .launch(new BrowserType.LaunchOptions().setHeadless(true).setArgs(List(
+        //https://github.com/microsoft/playwright/issues/8161#issuecomment-3643962063
+        "--disable-gpu",
+        "--disable-font-subpixel-positioning",
+        "--disable-lcd-text",
+        "--disable-threaded-animation",
+        "--disable-threaded-scrolling",
+        "--disable-in-process-stack-traces",
+        "--disable-checker-imaging",
+        "--force-color-profile=srgb"
+      ).asJava))
   }
 
   override def afterAll(): Unit = {
